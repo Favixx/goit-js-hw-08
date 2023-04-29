13c001cbf5f9e69d4f01f620cf5c11492e282f58
Favixx <bogdandom52@gmail.com>
2023-04-29 15:47:54 +0300

    listener fixes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems.map(el => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+        <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+    </a>
+    </li>`
+).join('');
+galleryList.innerHTML = galleryItemsMarkup;
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionsPosition: 'bottom'
+});
